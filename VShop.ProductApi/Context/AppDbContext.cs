3438818e75cafb9615acc73eb2eb6ab5212c2c74
Michel Matos <michel.mef@gmail.com>
2022-08-07 16:09:24 -0400

    product-criação

---
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Context;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category>? Categories {get; set;}
+    public DbSet<Product>? Products {get; set; }
+}
